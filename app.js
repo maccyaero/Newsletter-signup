0b13fe8393d99ac2587975b52ad3b342af643a6f
maccyaero <17176445+maccyaero@users.noreply.github.com>
2023-02-20 16:05:31 +1100

    Post User Data in a list in Mailchimp Server using Mailchimp API

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
-const app = express()
+const { json } = require("body-parser");
+const app = express();
+const https = require('https');
+const MailChimpKey = "52752642508897bb20a29e087e4bb029-us21";
+const AudienceID = "0156853c26";
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/signup.html");
@@ -16,7 +19,36 @@ app.post('/',function(req,res){
     var email = req.body.email;
 
     console.log(firstName, lastName, email)
-    
+    // Mailchimp API receives Post in this format:
+    var data = { 
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    //Converting JS Object to JSON object to send it to MailChimp
+    var jsonData = JSON.stringify(data);
+    // Data is ready now, time to make a POST request to Mailchimp 
+    const url = "https://us21.api.mailchimp.com/3.0/lists/"+AudienceID;
+    const options = {
+        method:"POST",
+        auth: "waliabbas:"+MailChimpKey,
+    };
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data))
+
+        })
+
+    });
+    request.write(jsonData);
+    request.end();
 });
 app.listen(3000, function(){
     console.log("Server Running on port 3000.")
